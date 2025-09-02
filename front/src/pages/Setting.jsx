1126e5bd3cc8c81130d4a6faf771e07a50a21f71
Junhyuk K <kjh27256901@gmail.com>
2025-09-02 22:25:43 +0900

    main1

---
+++ front/src/pages/Setting.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Setting = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold">Setting</h1>
+      <p>여기에 설정 페이지 내용을 추가하세요.</p>
+    </div>
+  );
+};
+
+export default Setting;
